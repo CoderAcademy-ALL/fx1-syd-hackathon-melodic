e8c90e39f4b037efef0ef3b156de2a541e61255b
June Du <junedu416@gmail.com>
2021-09-20 17:53:02 +1000

    test fetch

--- index.js
+++ index.js
@@ -1,10 +1,21 @@
-import axios from 'axios'
+// import axios from 'axios'
+
 
 const listUsers = async () => {
     try {
-        const res = await axios.get('api.genius.com/search?q=Kendrick%20Lamar');
-        console.log(res.data.data);
+        let res = await fetch('api.genius.com/artists/16775/songs');
+        let data = await res.json();
+        console.log(data);
     } catch (err) {
         console.error(err);
     }
 };
+
+
+const searchBtn = document.getElementById("search-btn");
+
+searchBtn.addEventListener("click", () => {
+    console.log("hello");
+    listUsers();   
+});
+

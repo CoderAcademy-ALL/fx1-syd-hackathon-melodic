48863b941e177944e57fba82e9f521d297211463
Ethan <disdroido@gmail.com>
2021-09-20 16:59:35 +1000

    changes to code

--- index.js
+++ index.js
@@ -1,24 +1,10 @@
-// var axios = require('axios');
-import { getLyrics, getSong } from './genius-lyrics-api/index.js';
+import axios from 'axios'
 
-const options = {
-	apiKey: '2OTy71eWKNpsyDIKMTw8B5UM45fdWwVJMtJlVJYzQ1QUgyOtRShxnJ76ZLJ5Mq1r',
-	title: 'Blinding Lights',
-	artist: 'The Weeknd',
-	optimizeQuery: true
+const listUsers = async () => {
+    try {
+        const res = await axios.get('api.genius.com/search?q=Kendrick%20Lamar');
+        console.log(res.data.data);
+    } catch (err) {
+        console.error(err);
+    }
 };
-
-const searchBtn = window.document.querySelector("#search-btn");
-
-searchBtn.addEventListener('click', () => {
-    getLyrics(options).then((lyrics) => console.log(lyrics));
-
-	getSong(options).then((song) =>
-		console.log(`
-		${song.id}
-		${song.title}
-		${song.url}
-		${song.albumArt}
-		${song.lyrics}`)
-	);
-})

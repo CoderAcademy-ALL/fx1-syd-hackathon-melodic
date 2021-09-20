9742ca08e0fb7d9feee1668034845496689fbdd4
June Du <junedu416@gmail.com>
2021-09-20 20:15:46 +1000

    render retrieved data on index page

--- index.js
+++ index.js
@@ -1,21 +1,52 @@
-// import axios from 'axios'
+function displaySongs(data){
+    const div = document.createElement("div")
+    if(data.length === 0) {
+        div.innerText = "Result not found..."
+        document.body.appendChild(div);
+    }
+    for (var i = 0; i < data.length; i++){
+        div.classList.add("card", "align-items-center");
+        var thumbnail = document.createElement("img");
+        thumbnail.src = data[i].header_image_thumbnail_url;
+        thumbnail.style.height = '300px';
+        thumbnail.style.width = '300px';
+        var a = document.createElement("a");
+        a.innerHTML += `${data[i].full_title}`
+        a.href = data[i].url;
+        div.appendChild(thumbnail);
+        div.appendChild(a);
+    }
+    document.body.appendChild(div);
+}
 
-
-const listUsers = async () => {
-    try {
-        let res = await fetch('api.genius.com/artists/16775/songs');
-        let data = await res.json();
-        console.log(data);
-    } catch (err) {
-        console.error(err);
+var options = {
+    method: 'GET',
+    url: 'https://genius.p.rapidapi.com/artists/16775/songs',
+    headers: {
+        'x-rapidapi-host': 'genius.p.rapidapi.com',
+        'x-rapidapi-key': 'ee685e6aadmshabc472a5750b606p1b5ea4jsn162990b75a3b'
     }
 };
 
-const searchBtn = document.getElementById("search-btn");
+const getData = () =>{
+    axios.request(options).then(function (res) {
+        let rawData = res.data.response.songs;
+        displaySongs(rawData)
+    }).catch(function (err) {
+        console.error(err);
+    });
+}
 
-searchBtn.addEventListener("click", () => {
-    console.log("hello");
-    listUsers();   
-});
+
+const searchBtn = document.getElementById("search-btn");
+searchBtn.addEventListener("click", getData());
+
+
+
+
+
+
+
+    
 
 
